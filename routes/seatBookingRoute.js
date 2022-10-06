c7c91de325f5b426d0862b2ad42f5b61d2e21d02
dzakia-st3 <annisadzakiaramadhina22@gmail.com>
2022-10-06 11:03:49 +0700

    feat : update seat booking

--- routes/seatBookingRoute.js
+++ routes/seatBookingRoute.js
@@ -2,7 +2,7 @@ const express = require("express")
 const seatBookingController = require("../controller/seatBookingController")
 const router = express.Router()
 
-router.get("/:id", seatBookingController.getSeat)
+router.get("/", seatBookingController.getSeat)
 router.post("/", seatBookingController.addSeat)
 router.patch("/:id", seatBookingController.updateSeat)
 router.delete("/:id", seatBookingController.removeSeat)
